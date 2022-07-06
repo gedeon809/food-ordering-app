8c89749e0edeb47f6bd644240b6cb91b37916eba
Gedeon Kakona <gedeonkakona@gmail.com>
2022-07-06 09:53:38 +0200

    Adding cookie

--- pages/api/products/[id].js
+++ pages/api/products/[id].js
@@ -5,7 +5,9 @@ export default async function handler(req, res) {
   const {
     method,
     query: { id },
+    cookies,
   } = req;
+  const token = cookies.token;
 
   dbConnect();
 
@@ -17,19 +19,28 @@ export default async function handler(req, res) {
       res.status(500).json(err);
     }
   }
+
   if (method === 'PUT') {
+    if (!token || token !== process.env.token) {
+      return res.status(401).json('Not authenticated!');
+    }
     try {
-      const product = await Product.findByIdAndUpdate(id, req.body);
-      res.status(201).json(product);
+      const product = await Product.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json(product);
     } catch (err) {
       res.status(500).json(err);
     }
   }
 
   if (method === 'DELETE') {
+    //if (!token || token !== process.env.token) {
+    // return res.status(401).json('Not authenticated!');
+    //}
     try {
-      const product = await Product.create(req.body);
-      res.status(201).json(product);
+      await Product.findByIdAndDelete(id);
+      res.status(200).json('The product has been deleted!');
     } catch (err) {
       res.status(500).json(err);
     }
--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -2,7 +2,9 @@ import dbConnect from '../../../util/mongo';
 import Product from '../../../models/Product';
 
 export default async function handler(req, res) {
-  const { method } = req;
+  const { method, cookies } = req;
+
+  const token = cookies.token;
 
   dbConnect();
 
@@ -14,7 +16,11 @@ export default async function handler(req, res) {
       res.status(500).json(err);
     }
   }
+
   if (method === 'POST') {
+    if (!token || token !== process.env.token) {
+      return res.status(401).json('Not authenticated!');
+    }
     try {
       const product = await Product.create(req.body);
       res.status(201).json(product);
